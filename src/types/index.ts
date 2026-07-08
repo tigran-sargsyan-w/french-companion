b17a3c278ea41d0fb9fe65df8ee150425d617045
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-08 22:03:49 +0200

    Add shared type exports

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type { VocabStatus, VocabWord } from "./vocabulary";
+export type { GrammarTopic } from "./grammar";
+export type { MistakeCategory, Mistake } from "./mistake";
+export type { HomeworkItem } from "./homework";
+export type { Lesson } from "./lesson";
