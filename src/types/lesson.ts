2f3db988c70eea041170f49c90dd41bf525727c0
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-08 22:03:44 +0200

    Add lesson domain types

---
+++ src/types/lesson.ts
@@ -0,0 +1,11 @@
+export interface Lesson {
+  id: string;
+  title: string;
+  date: string;
+  summary: string;
+  grammarTopicIds: string[];
+  vocabIds: string[];
+  homeworkIds: string[];
+  notes: string;
+  photos: string[];
+}
