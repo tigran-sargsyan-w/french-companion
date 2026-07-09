453c44f76e72dd73f632058c24d39b98bb115875
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-09 21:31:49 +0200

    Remove redundant lesson content id lists from type

--- src/types/lesson.ts
+++ src/types/lesson.ts
@@ -3,9 +3,6 @@ export interface Lesson {
   title: string;
   date: string;
   summary: string;
-  grammarTopicIds: string[];
-  vocabIds: string[];
-  homeworkIds: string[];
   notes: string;
   photos: string[];
 }
