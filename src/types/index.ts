57f8381fc881f22462f0a97ecdf0d088d194c0f9
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-09 20:07:42 +0200

    Export vocabulary source item type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type { VocabStatus, VocabWord } from "./vocabulary";
+export type { VocabSourceItem, VocabStatus, VocabWord } from "./vocabulary";
 export type { GrammarTopic } from "./grammar";
 export type { MistakeCategory, Mistake } from "./mistake";
 export type { HomeworkItem } from "./homework";
