65be74cc21212791d8ba4250067284dca68d6004
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-09 23:46:23 +0200

    Export lesson photo types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,4 @@ export type { VocabSourceItem, VocabStatus, VocabWord } from "./vocabulary";
 export type { GrammarTopic } from "./grammar";
 export type { MistakeCategory, Mistake, MistakeSourceItem } from "./mistake";
 export type { HomeworkItem, HomeworkSourceItem } from "./homework";
-export type { Lesson } from "./lesson";
+export type { Lesson, LessonPhoto, LessonPhotoObject } from "./lesson";
